e14496824a97b5f36239a45ee09fb7b10044bed0
davi-pc <davimatosc@hotmail.com>
2025-07-31 03:36:30 -0300

    First commit

---
+++ src/main/java/br/com/uema/davimc/SafeUber/SafeUberApplication.java
@@ -0,0 +1,13 @@
+package br.com.uema.davimc.SafeUber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeUberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uema/davimc/SafeUber/SafeUberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uema.davimc.SafeUber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
